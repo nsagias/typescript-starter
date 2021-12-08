0d412d9590b3f61a8e34f027b72121281f0fd13e
Nick <nsagias@gmail.com>
2021-12-08 02:18:29 -0500

    Add arrays with types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 // Basic Types
 let id: number = 3;
-let appName: string = "paykids";
+let appName: string = "Hello World";
 let isForKids: boolean = true;
 let x: any = "Set to either anything";
+
+// Arrays
+let arrNumsOnly: number[] = [1,2,3,4,5];
+let arrStringsOnly: string[] = ['name', 'lastName', 'Hello'];
+let arrAny     : any [] = [1, true, "hello world"];
